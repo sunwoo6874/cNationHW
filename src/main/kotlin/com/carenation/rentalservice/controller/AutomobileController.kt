3e4b35a3670c58d326df6cc37cb7b3fe8a4644bc
sunwoo_baek <sunwoo.baek@llsollu.com>
2025-03-07 02:35:39 +0900

    change root api, update index.html

--- src/main/kotlin/com/carenation/rentalservice/controller/AutomobileController.kt
+++ src/main/kotlin/com/carenation/rentalservice/controller/AutomobileController.kt
@@ -5,6 +5,7 @@ import org.springframework.http.*
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/rental/api")
 class AutomobileController(private val automobileService: AutomobileService) {
 
     @PostMapping("/register")
